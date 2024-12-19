7bc02dbf32cfc29f07a56ba1d8b510440f5ad117
AkhilP2003 <akhil.pindiprolu@mail.utoronto.ca>
2024-12-18 23:12:23 -0500

    linked proj to db

---
+++ src/main/java/dev/akhil/movies/MoviesApplication.java
@@ -0,0 +1,24 @@
+package dev.akhil.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication // this annotation is needed in every MAIN class of every spring boot app (it automatically
+// configures the components based on dependencies and treats MAIN file as the configuration file)
+
+@RestController // annotation for a web controller that handles http requests but returns a json obj
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@GetMapping // annotation lets the framework know that this method is a GET req endpoint
+	public String apiRoot() {
+		return "Welcome to the Movies API";
+	}
+
+}
